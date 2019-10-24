3f299b5770deb396898eb6a38b752abbd7cbc44e
Thien Nguyen <nhatthien185@gmail.com>
2019-10-24 20:15:32 +0700

    Add module cat and property eatCat for Dog

--- Dog.js
+++ Dog.js
@@ -2,7 +2,12 @@ var chalk = require('chalk');
 
 function Dog(name) {
 	this.name = name;
+	this.stomach = [];
 }
+
+Dog.prototype.eatCat = function(cat) {
+	this.stomach.push(cat);
+};
 
 Dog.prototype.sayHi = function() {
 	console.log('Hi, welcome to my house, I am ' + chalk.red(this.name));
