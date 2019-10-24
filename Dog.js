be2cf6e7475b72411ce978b33c1d2410ea61bd34
Thien Nguyen <nhatthien185@gmail.com>
2019-10-24 14:32:45 +0700

    Add something new

--- Dog.js
+++ Dog.js
@@ -1,3 +1,11 @@
-function Dog() {
-	
+var chalk = require('chalk');
+
+function Dog(name) {
+	this.name = name;
 }
+
+Dog.prototype.sayHi = function() {
+	console.log('Hi, welcome to my house, I am ' + chalk.red(this.name));
+};
+
+module.exports = Dog;
---
+++ index.js
@@ -0,0 +1,5 @@
+var Dog = require('./Dog.js');
+	
+var dog1 = new Dog('Tom');
+
+dog1.sayHi();
