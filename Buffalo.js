db3e7d50a644edb74facf38130d8f2af2dd12fc2
Thien Nguyen <nhatthien185@gmail.com>
2020-03-21 20:59:28 +0700

    run method

--- Buffalo.js
+++ Buffalo.js
@@ -6,4 +6,8 @@ Buffalo.prototype.sayHi = function() {
   console.log("He he he he he");
 };
 
+Buffalo.prototype.run = function() {
+  console.log("Run run run run");
+};
+
 module.exports = Buffalo;
