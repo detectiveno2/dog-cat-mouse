8a90c588db3b07f807574d9719704d9240ca60af
Thien Nguyen <nhatthien185@gmail.com>
2020-03-21 16:26:27 +0700

    Edit buffalo constructor and add method sayHi

--- Buffalo.js
+++ Buffalo.js
@@ -1,3 +1,9 @@
-function Buffalo() {
-	
+function Buffalo(name) {
+  this.name = name;
 }
+
+Buffalo.prototype.sayHi = function() {
+  console.log("He he he he he");
+};
+
+module.exports = Buffalo;
